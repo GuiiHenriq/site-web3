d978cc004aebe2e1622212840a33a0c4a09e1cc8
Guilherme Henrique <guilhermeerba@hotmail.com>
2022-05-14 11:24:30 -0300

    allow sending of messages and others data

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,17 +7,35 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver; // Endereço do usuário que deu tchauzinho
+        string message; // Mensagem que o usuário envio
+        uint256 timestamp; // Data/hora de quando o usuário tchauzinhou.
+    }
+
+    Wave[] waves;
+
     constructor() {
         console.log("My first smart contract!!");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s deu tchauzinho!", msg.sender);
+        console.log("%s tchauzinhou com a mensagem %s", msg.sender, _message);
+        
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
-        console.log("Temos um total de %d tchauzinhos!", totalWaves);
+        console.log("Temos %d tchauzinhos no total!", totalWaves);
         return totalWaves;
     }
 }
